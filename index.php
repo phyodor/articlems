d4f7c34b96ddcce7760252040e8391c756543565
unknown <alex.fedorashko@.dotdigitalgroup.plc>
2011-08-02 11:52:16 +0300

    css index

--- index.php
+++ index.php
@@ -2,16 +2,29 @@
 <html>
 <head>
 	<title>Article Management System</title>
+<link type="text/css" href="/css/1.css" rel="stylesheet" media="screen" />
 </head>
 
 <body>
-
+<div id="submit_form">
+<b>Submit Your Article:</b><br />
 <form action="article_controller.php" method="post">
-Title: <input type="text" name="title" /><br />
-Author: <input type="text" name="author" /><br />
+Title: <input type="text" name="title" id="title" /><br />
+Author: <input type="text" name="author" />
 Date: <input type="date" name="date" /><br />
-<textarea name="body"></textarea>
+Excerpt:<br />
+<textarea id="excerpt" name="excerpt"></textarea><br />
+Article:<br />
+<textarea id="body" name="body"></textarea><br />
+Choose Category:<br />
+Business <input type="checkbox" value="category" name="business" />
+Tech <input type="checkbox" value="category" name="tech"/>
+Science <input type="checkbox" value="category" name="science"/>
+Automotive <input type="checkbox" value="category" name="automotive"/><br />
+<input type="submit" value="Publish" />
 </form>
+</div>
+
 <?php
 
 ?>
